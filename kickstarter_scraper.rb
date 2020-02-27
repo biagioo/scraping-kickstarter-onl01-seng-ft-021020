5429aefddaddc215a7c17490b830a1978af342fa
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-27 15:48:45 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta").text
-
+# location: project.css("ul.project-meta span.location-name").text
 
 
 
